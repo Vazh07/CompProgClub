5bc54c356b4675c09b956837e5aae129ee2fc49a
Vazh07 <mozamorah@gmail.com>
2024-02-19 15:48:40 -0600

    Solution for 18490

--- OCI2023/18490.Tortillas_horneadas/sol.cpp
+++ OCI2023/18490.Tortillas_horneadas/sol.cpp
@@ -1,1 +1,15 @@
 // Solution for problem in 18490.Tortillas_horneadas
+#include <iostream>
+
+int solve(int a, int b){
+    //un gramo de maíz por cada centímetro cuadrado de la tortilla
+    a+=5;
+    b+=3;
+    return a*b;
+}
+
+int main(){
+    int solution = solve(10,7);
+    std::cout<<solution<<'\n';
+    return 0;
+}
